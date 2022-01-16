dfd7d82c27cc744d7bb289df94e08fb53ba04728
LukasKalit <lukaskalit@gmail.com>
2022-01-17 00:16:37 +0100

    Add readme.

---
+++ README.md
@@ -0,0 +1,4 @@
+#Creating a Spotify Playlist using the Musical Time Machine
+
+##Program scrape from website 100 the most popular songs from specific date and create on spotify account a playlist of this songs.
+
--- main.py
+++ main.py
@@ -7,45 +7,70 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import pprint
 
-load_dotenv()
 
+
+#                 Environment Variable
+load_dotenv()
 Client_ID = os.getenv("Client_ID")
 Client_Secret = os.getenv("Client_Secret")
 
+#                  Connection to spotify
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=Client_ID,
                                                client_secret=Client_Secret,
-                                               redirect_uri="http://example.com"))
+                                               redirect_uri="http://example.com",
+                                               scope="playlist-modify-private"))
+user_name = sp.current_user()["id"]
+print(user_name)
 
-# date = input("Chose a day you want to travel YYYY-MM-DD: ")
+date = input("Chose a day you want to travel YYYY-MM-DD: ")
 # print(date)
 
-year = "2010"
-html_doc = f"https://www.billboard.com/charts/hot-100/2010-12-27/"
+
+#                Taking data from site
+html_doc = f"https://www.billboard.com/charts/hot-100/{date}/"
 
 response = requests.get(html_doc)
-response.status_code
+response.raise_for_status()
 
 soup = BeautifulSoup(response.text, "html.parser")
 
+#                 Scraping
 links = soup.find_all(class_="o-chart-results-list-row-container")
 list_of_titles = [row.find("h3").string.strip() for row in links]
-list_of_artists = [row.find("span", ["c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max u-line-height-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellipsis-2line u-max-width-330 u-max-width-230@tablet-only", "c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max u-line-height-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellipsis-2line u-max-width-330 u-max-width-230@tablet-only u-font-size-20@tablet"]).text.strip().split()[0] for row in links]
+list_of_artists = [row.find("span", ["c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max"
+                                     " u-line-height-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block"
+                                     " a-truncate-ellipsis-2line u-max-width-330 u-max-width-230@tablet-only",
+                                     "c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max u-line-height"
+                                     "-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellipsis"
+                                     "-2line u-max-width-330 u-max-width-230@tablet-only u-font-size-20@tablet"]
+                            ).text.strip().split()[0] for row in links]
 
 # print(links)
 # print(list_of_titles)
 # print(list_of_artists)
 
+#              Searching for songs in spotify
+
+list_of_uri = []
 
 for i in range(100):
     query = f"track:{list_of_titles[i]} AND artist:{list_of_artists[i]}"
-    playlists = sp.search(q=query, type="track", limit=1)
+    play_search = sp.search(q=query, type="track", limit=1)
     # pprint.pprint(playlists)
 
     try:
-        pprint.pprint(playlists["tracks"]["items"])
-        for j in range(10):
-            pprint.pprint(playlists["tracks"]["items"][j]["name"])
-            pprint.pprint(playlists["tracks"]["items"][j]["external_urls"]["spotify"])
+        # pprint.pprint(play_search["tracks"]["items"][0]["name"])
+        # pprint.pprint(play_search["tracks"]["items"][0]["uri"])
+        list_of_uri.append(str(play_search["tracks"]["items"][0]["uri"].split(":")[2]))
 
     except:
         pass
+
+#               Create a playlist and save 100 songs on private account
+playlist_ID = sp.user_playlist_create(user=user_name, name=f"Top 100 of {date}", public=False,
+                                      description="Practising of using spotipy module")
+
+sp.playlist_add_items(playlist_id=playlist_ID["id"], items=list_of_uri)
+
+# pprint.pprint(playlist_ID)
+# print(list_of_uri)
